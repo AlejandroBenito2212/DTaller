a30f7daad28d495513f99c1492189460ab00d4a0
AlejandroBenito2212 <128435898+AlejandroBenito2212@users.noreply.github.com>
2023-03-20 22:13:06 -0400

    Update Taller.java

--- Taller.java
+++ Taller.java
@@ -13,21 +13,27 @@ public class taller{
         Numero=0;
         Fecha="";
     }
-
+/*
+ * 
+ */
     public void leer(){
         //TODO: INGRESAR DATOS DEL TALLER
     }
     public void imprimir(){
         //TODO: MOSTRAR LOS DATOS DEL TALLER
     }
-
+/*
+ * 
+ */
     public String getNombre(){
         if(Nombre="")
         return "error no tiene nombre";
         else
         return Nombre;
     }
-
+/*
+ * 
+ */
     public void setFecha(String fecha){
         this.Fecha = Fecha;
     }
