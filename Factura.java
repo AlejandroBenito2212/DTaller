8e45af18aee454da8a8f5ce522aa81d278c336a9
AlejandroBenito2212 <128435898+AlejandroBenito2212@users.noreply.github.com>
2023-03-20 22:11:49 -0400

    Update Factura.java

--- Factura.java
+++ Factura.java
@@ -3,17 +3,42 @@ import java.lang.*;
  * 
  */
 public class Factura{
-    private String fecha;
-    private int numero;
-    private String impite;
+    private String Fecha;
+    private int Numero;
+    private String Impite;
 /*
  * 
  */
     public Factura(){
-        fecha="";
-        numero=0;
-        impite="";
+        Fecha="";
+        Numero=0;
+        Impite="";
     }
-
-    
+    /*
+     * 
+    */
+    public void leer(){
+        //TODO: INGRESAR DATOS DEL LA FACTURA
+    }
+    /*
+     * 
+     */
+    public void imprimir(){
+        //TODO: MOSTRAR LOS DATOS DEL FACTURA
+    }
+    /*
+     * 
+     */
+    public int getNumero(){
+        if(Numero=0)
+        return "ERROR LA FACTURA NO EXISTE";
+        else
+        return Numero;
+    }
+    /*
+     * 
+     */
+    public void setFecha(String fecha){
+        this.Fecha = Fecha;
+    }
 }
