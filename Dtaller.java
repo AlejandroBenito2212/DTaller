458233c713d42a1c0830a797f140779f2824824c
AlejandroBenito2212 <128435898+AlejandroBenito2212@users.noreply.github.com>
2023-03-20 22:10:09 -0400

    Update Dtaller.java

--- Dtaller.java
+++ Dtaller.java
@@ -1,10 +1,23 @@
-public class DTaller{
+public class Dtaller{
 
     public static void main(String[] args) {
-        Talelr taller;//
-        taller = new taller();
+        Taller taller;//
+        Factura factura;//
+        Vehiculo vehiculo;//
+        Propietario propietario;//
+        taller = new Taller();
+        factura = new Factura();
+        vehiculo = new Vehiculo();
+        propietario = new Propietario();
+
         
         taller.leer();
         taller.imprimir();
+        factura.leer();
+        factura.imprimir();
+        vehiculo.leer();
+        vehiculo.imprimir();
+        propietario.leer();
+        propietario.imprimir();
     }
 }
